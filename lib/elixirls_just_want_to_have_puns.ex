ce4dc2c99907b8b0498deca2e45e5846c5979593
Jason Draper <jason@drapergeek.com>
2015-10-09 12:57:17 -0700

    Make RhymebrainResult printable and show max score

--- lib/elixirls_just_want_to_have_puns.ex
+++ lib/elixirls_just_want_to_have_puns.ex
@@ -3,6 +3,12 @@ defmodule RhymebrainResult do
   defstruct [:word, :score]
 end
 
+defimpl String.Chars, for: RhymebrainResult do
+  def to_string(rhymebrain_result) do
+    "word: #{rhymebrain_result.word} / score: #{rhymebrain_result.score}"
+  end
+end
+
 defmodule ElixirlsJustWantToHavePuns do
   def fetch(word) do
     HTTPoison.start
@@ -17,6 +23,11 @@ defmodule ElixirlsJustWantToHavePuns do
 
   def handle_response({:ok, %{status_code: 200, body: body}}) do
     Poison.decode(body, as: [RhymebrainResult])
+    |> max_score
+  end
+
+  def max_score({:ok, results}) do
+    Enum.max_by(results, &(&1.score))
   end
 end
 
