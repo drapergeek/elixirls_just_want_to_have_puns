1af0c9e114b6c92815c88203f5efc12aa681bd89
Jason Draper <jason@drapergeek.com>
2015-10-09 12:51:37 -0700

    Decode raw JSON into Elixir structs

--- lib/elixirls_just_want_to_have_puns.ex
+++ lib/elixirls_just_want_to_have_puns.ex
@@ -1,5 +1,9 @@
-defmodule ElixirlsJustWantToHavePuns do
+defmodule RhymebrainResult do
+  @derive [Poison.Encoder]
+  defstruct [:word, :score]
+end
 
+defmodule ElixirlsJustWantToHavePuns do
   def fetch(word) do
     HTTPoison.start
     rhymebrain_url(word)
@@ -11,9 +15,8 @@ defmodule ElixirlsJustWantToHavePuns do
     "http://rhymebrain.com/talk?function=getRhymes&word=#{word}&maxResults=0&lang=en"
   end
 
-  def handle_response(_) do
-    IO.puts "SUCCESS"
-    # { :ok, :jsx.decode(body) }
+  def handle_response({:ok, %{status_code: 200, body: body}}) do
+    Poison.decode(body, as: [RhymebrainResult])
   end
 end
 
